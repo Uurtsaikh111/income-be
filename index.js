2a1fc02d234408f45c4deb99f5e0572dc2c98159
Үүрцайх Норовсамбуу <23lp8969@nest.edu.mn>
2024-03-12 10:59:59 +0800

    Mar12

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-const { Pool } = require("pg");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const { addUser } = require("./route/add-user");
+const router = express.Router();
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
+router.post("/add-user", addUser);
 
-const pgConif = {
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: PGPORT,
-  ssl: {
-    require: true,
-  },
-};
-
-const pool = new Pool(pgConif);
+app.use(router);
 
 // async function getPgVersion() {
 //   const client = await pool.connect();
@@ -52,8 +41,6 @@ const pool = new Pool(pgConif);
 //   }
 // }
 
-
-
 // app.get("/users", async (req, res) => {
 //   const client = await pool.connect();
 //   try {
@@ -66,54 +53,52 @@ const pool = new Pool(pgConif);
 //   }
 // });
 
-app.post("/add-user", async (req, res) => {
-  const newUser = req.body;
-  console.log(newUser);
-  const client = await pool.connect();
-  const Query = `INSERT INTO users (name, email, id, password, currency) VALUES ('${newUser.name}','${newUser.email}','${newUser.id}','${newUser.password}','${newUser.currency}');`;
-  try {
-    const response = await client.query(Query);
-    console.log("response", { response });
-    if (response["rowCount"]) {
-      console.log(response["rowCount"])
-      return res.status(200).send({ success: "true" });
-      
-    } else {
-      console.log(response["rowCount"]);
-      return res.status(500).send({ success: "false" });
-    }
-  } catch (e) {
-    console.log(e);
-  } finally {
-    client.release();
-  }
+// app.post("/add-user", async (req, res) => {
+//   const newUser = req.body;
+//   console.log(newUser);
+//   const client = await pool.connect();
+//   const Query = `INSERT INTO users (name, email, id, password, currency) VALUES ('${newUser.name}','${newUser.email}','${newUser.id}','${newUser.password}','${newUser.currency}');`;
+//   try {
+//     const response = await client.query(Query);
+//     //console.log("response", { response });
+//     if (response["rowCount"]) {
+//       //console.log(response["rowCount"]);
+//       return res.status(200).send({ success: "true" });
+//     } else {
+//       //console.log(response["rowCount"]);
+//       return res.status(500).send({ success: "false" });
+//     }
+//   } catch (e) {
+//     console.log(e);
+//     return res.status(500).send({ success: "false" });
+//   } finally {
+//     client.release();
+//   }
+// });
 
-});
+// app.post("/login", async (req, res) => {
+//   const user = req.body;
+//   console.log("user", user);
+//   const client = await pool.connect();
+//   const Query = `SELECT * FROM users WHERE (email='${user.email}' AND password='${user.password}');`;
 
-app.post("/login", async (req, res) => {
-  const user = req.body;
-  console.log("user", user);
-  const client = await pool.connect();
-  const Query = `SELECT * FROM users WHERE (email='${user.email}' AND password='${user.password}');`;
-
-  try {
-    const response = await client.query(Query);
-    console.log("response", { response });
-    if (response["rowCount"]) {
-      console.log(response["rowCount"])
-      return res.status(200).send({ success: "true" });
-      
-    } else {
-      console.log(response["rowCount"]);
-      return res.status(500).send({ success: "false" });
-    }
-  } catch (e) {
-    console.log(e);
-  } finally {
-    client.release();
-    console.log("user add successfully");
-  }
-});
+//   try {
+//     const response = await client.query(Query);
+//     console.log("response", { response });
+//     if (response["rowCount"]) {
+//       console.log(response["rowCount"]);
+//       return res.status(200).send({ success: "true" });
+//     } else {
+//       console.log(response["rowCount"]);
+//       return res.status(500).send({ success: "false" });
+//     }
+//   } catch (e) {
+//     console.log(e);
+//   } finally {
+//     client.release();
+//     console.log("user add successfully");
+//   }
+// });
 
 // app.delete("/delete-user", async (req, res) => {
 //   const deleteUser = req.body;
