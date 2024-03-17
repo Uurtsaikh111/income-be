72fa2f298700adacd182787362e838490ef10a32
Uurtsaikh <uuree.i4uuree@gmail.com>
2024-03-17 14:53:06 +0800

    Category

--- route/category.js
+++ route/category.js
@@ -1,11 +1,22 @@
-const { addCategories } = require("../service/category-service");
+const { addCategories, getCategories, deleteCategory } = require("../service/category-service");
 
 const categoryRouter = require("express").Router();
 
-categoryRouter.post("/add-categories", async (req, res) => {
+categoryRouter.post("/add-category", async (req, res) => {
     const newUserData = req.body;
     const result = await addCategories(newUserData);
     res.json(result);
+  });
+
+  categoryRouter.delete("/del-category", async (req, res) => {
+    const newUserData = req.body;
+    const result = await deleteCategory(newUserData);
+    res.json(result);
+  });
+
+  categoryRouter.get("/categories", async (req, res) => {
+    const users = await getCategories();
+    res.json(users);
   });
 
   module.exports = categoryRouter;
--- service/category-service.js
+++ service/category-service.js
@@ -18,13 +18,11 @@ const pool = new Pool(pgConif);
 async function addCategories(newUser) {
     const client = await pool.connect();
     console.log(newUser)
-    const Query = `INSERT INTO category (name, description, id, createat, updatedat, category_image) VALUES ('${newUser.name}','${newUser.description}','${newUser.id}','${newUser.createdat}','${newUser.updatedat}','${newUser.category_image}');`;
-    //CREATE TABLE Category (name VARCHAR(50) NOT NULL, id VARCHAR(50) NOT NULL PRIMARY KEY, description TEXT, createdAt TIMESTAMP, updatedAt TIMESTAMP, category_image text )"
+    const Query = `INSERT INTO category (name, id, category_image) VALUES ('${newUser.name}','${newUser.id}','${newUser.icon}');`;
     let response;
   
     try {
       response = await client.query(Query);
-      console.log(response)
     } catch (e) {
     } finally {
       client.release();
@@ -36,8 +34,36 @@ async function addCategories(newUser) {
     }
   }
 
+  async function getCategories() {
+    const client = await pool.connect();
+    let response;
+    try {
+      response = await client.query("SELECT * FROM category");
+    } catch (error) {
+      throw new Error(error ? error.message : "Error");
+    } finally {
+      client.release();
+    }
+    return response.rows;
+  }
+
+  async function deleteCategory(cat) {
+    const client = await pool.connect();
+    const Query = `DELETE FROM category WHERE name='${cat.name}'`;
+    
+    let response;
+    try {
+      response = await client.query(Query);
+    } catch (e) {
+    } finally {
+      client.release();
+    }
+    return "deleted cat"
+  }
   module.exports = {
-    addCategories
+    addCategories,
+    getCategories,
+    deleteCategory
   };
   
 
